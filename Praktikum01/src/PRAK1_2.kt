a40e34194d71b515de096ee981d56d01374ca8eb
Arufff <101610166+Arufff@users.noreply.github.com>
2022-03-23 12:45:45 +0800

    Add files via upload

---
+++ Praktikum01/src/PRAK1_2.kt
@@ -0,0 +1,15 @@
+/*Mohon maaf ka/pak sebelumnya untuk nama praktikum no 2 ini tidak sesuai formatnya karena
+IDE saya error pak kalau pakai nama PRAK102 entah kenapa*/
+
+import java.util.Scanner
+
+fun main(){
+    var x = 0
+    val input = Scanner(System.`in`)
+
+    print("Nilai x = ")
+    x = input.nextInt()
+    val rumus = ((2 * (x * x)) + (5 * x) - 8)
+
+    print(rumus)
+}
